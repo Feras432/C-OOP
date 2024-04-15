c7e04ce544a3d29a9aa1b42376f8aed0a11ae709
firas <firasshdad@gmail.com>
2024-04-15 09:47:25 +0300

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using ConsoleApp1;
+
+var student = new Student("Faisal", 3.54, "Computer Engineering");
+student.FormatStudent();
+
+var gradStudent = new GraduateStudent("Fawaz", 3.76, "Ind. Engineering");
+gradStudent.FormatStudent();
+
---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   
+
+    public class GraduateStudent : Student
+    {
+        public GraduateStudent(string name, double gPA, string major) : base(name, gPA, major)
+        {
+            Console.WriteLine("Congratulations !!! " + name + " You've graduated!!!");
+        }
+
+        
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+        public double GPA { get; set; }
+        public string Major { get; set; } 
+        public Student(string name, double gPA, string major)
+        {
+            Name = name;
+            GPA = gPA;
+            Major = major;
+        }
+
+        public void FormatStudent()
+        {
+            Console.WriteLine("Welcome " + Name + " Your GPA is " + GPA + " And your major is " + Major);
+        }
+    }
+}
